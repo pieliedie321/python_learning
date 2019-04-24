24bdf8277879be4b9bea5ee913ec2a37d249270a
pieliedie321 <ilya.pukhonin@gmail.com>
2019-04-24 12:04:05 +0300

    Changed 1 line in ASCII_v1

--- ASCII_v1.py
+++ ASCII_v1.py
@@ -1,5 +1,5 @@
 print('Input the word and see whats happend')
 S = input('Input word ')
 for x in list(S):
-    print('ASCII ', x, ' = ', ord(x))
+    print('ASCII of ', x, ' = ', ord(x))
 input('Press any key to continue')
