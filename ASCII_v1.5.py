9fd76c0eab4f0e7c4470fccbc5dfe83d0eec441a
pieliedie321 <ilya.pukhonin@gmail.com>
2019-04-25 11:20:42 +0300

    Changing in ASCII exercises. Delete ASCII_v2 (working in ASCII_v1.5)

--- ASCII_v1.5.py
+++ ASCII_v1.5.py
@@ -1,13 +1,16 @@
 # First version released!
 # In second version, i'm adding new feature: Sum of all simbols in S
 print('Input the word and see whats happend')
-c = 0
-L = []
 S = input('Input word ')
+L = []
+
 for x in list(S):
-    print('ASCII of ', x, ' = ', ord(x))
-    print('Now see sum of all ASCII codes!')
-    c += ord(x)
-    print('Summ = ', c)
-    print('List of all codes are - ', L.append(ord(x)))
+    print('ASCII of ', x, ' => ', ord(x))
+    L.append(int(ord(x)))
+
+print('List of all codes are - ', L)
+
+a = sum(L)
+print('Sum = ', a)
+
 input('Press any key to continue')
--- ASCII_v1.py
+++ ASCII_v1.py
@@ -1,7 +1,6 @@
-# FIrst version released!
-# In second version, i'm adding new feature: Sum of all simbols in S
+# First version released!
 print('Input the word and see whats happend')
 S = input('Input word ')
 for x in list(S):
-    print('ASCII of ', x, ' = ', ord(x))
+    print('ASCII of ', x, ' => ', ord(x))
 input('Press any key to continue')
