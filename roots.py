0374125fcc9c47eb54402ce266b919b78d8e77fa
pieliedie321 <ilya.pukhonin@gmail.com>
2019-05-07 14:38:49 +0300

    roots v1.1

--- roots.py
+++ roots.py
@@ -4,7 +4,7 @@ b = float(input('Input b = '))
 c = float(input('Input c = '))
 D = b*b - 4*a*c
 if D > 0:
-    print('D > 0, 2 roots')
+    print('D = ', D, '. D > 0, 2 roots')
     x1 = (-b + math.sqrt(D)) / 2*a
     x2 = (-b - math.sqrt(D)) / 2*a
     print('Roots: ', x1, 'and ', x2)
