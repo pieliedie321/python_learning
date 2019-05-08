c9d791a2a3cec0c38b2f9deeb8cf7f0932a5ecf3
Ilya <ilya.pukhonin@gmail.com>
2019-05-08 19:02:44 +0300

    simple cicle v1.5

--- simple_cicle.py
+++ simple_cicle.py
@@ -3,7 +3,7 @@ cicle = 1
 while cicle == 1:
     a = input('a = ')
     b = input('b = ')
-    print(a + b)
+    print(int(a) + int(b))
     print('do again? (y/n)')
     x = input()
     if x == 'y':
